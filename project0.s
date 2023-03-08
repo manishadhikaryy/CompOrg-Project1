b2acb5b29947843b0b6d00bb5157623e6d91a134
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2023-03-08 16:00:01 -0500

    Update project0.s
    we loaded the 7th index element (6) to register $t1 and replaced the element with the previously stored element from the $t2 (8th index element) i.e 0. we loaded the 6th index element (4) in the $t2 register and replaced 4 with 6 from the previously stored value in $t1. In the last line we load the 5th index element to the register $t1.

--- project0.s
+++ project0.s
@@ -89,3 +89,21 @@ lb $t2, 8($t0)
 
 #storing the byte from register $t1 to the memory address 8($t0) i.e. 8th index
 sb $t1, 8($t0)
+
+#commit
+##229689460
+
+#loading the byte in the memory address 7($t0) i.e. 7th index to register $t1
+lb $t1, 7($t0)
+
+#storing the byte from register $t2 to the memory address 7($t0) i.e. 7th index
+sb $t2, 7($t0)
+
+#loading the byte in the memory address 6($t0) i.e. 6th index to the register $t2
+lb $t2, 6($t0)
+
+#storing the byte from register $t1 to the memory address 6($t0) i.e. 6th index
+sb $t1, 6($t0)
+
+#loading the byte in the memory address 5($t0) i.e. 5th index to the register $t1
+lb $t1, 5($t0)

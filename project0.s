0471cd0cf4b8587e4f74b6be31918e9ef21866e6
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2023-03-08 18:27:55 -0500

    Update project0.s
    loaded the element 9 (index 2) in the register $t2. Replaced the earlier stored value of index 3 (8) to the 9 in the index 2. also replaced the value of index 1 with the 9 loaded in the register $t2

--- project0.s
+++ project0.s
@@ -126,3 +126,16 @@ lb $t1, 3($t0)
 #storing the byte from register $t2 to the memory address 3($t0) i.e 3rd index
 sb $t2, 3($t0)
 
+#commit
+#2298946@0
+
+#loading the byte in the memory address 2($t0) i.e. 2nd index to register $t2
+lb $t2, 2($t0)
+
+#storing the byte from register $t1 to the memory address 2($t0) i.e. 2nd index
+sb $t1, 2($t0)
+
+#storing the byte from register $t1 to the memory address 1($t0) i.e. 1st index
+sb $t2, 1($t0)
+
+

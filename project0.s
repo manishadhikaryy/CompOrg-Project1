064dff318a99b71428f56405cfb5ad5a37dd34fe
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2023-03-08 09:48:35 -0500

    Update project0.s
    Replaced the element in 6th index with the 7th indexed element we preserved in $t1 above, but, at the same time, we also have preserved the initial 6th indexed element in the register $t2 in the last line of the previous commit. we load the 5th index element to the register $t1 to preserve the value, while we replace the element with the element in the 6th index. Then we loaded the element in 4th index to the register $t2 and replaced the element by the 5th index element we stored in $t1 earlier.

--- project0.s
+++ project0.s
@@ -17,6 +17,7 @@ lb $t2, 1($t0)
 #storing byte from the register $t2 to the memory address 0($t0) i.e. 0th index
 sb $t2, 0($t0)
 
+##commit
 #loading the byte in the memory address 8($t0) i.e 8th index to register $t2
 lb $t2, 8($t0)
 
@@ -31,3 +32,21 @@ sb $t2, 7($t0)
 
 #loading the byte in the memory address 6($t0) i.e. 6th index to the register $t2
 lb $t2, 6($t0)
+
+##commit
+#storing the byte from register $t1 to the memory address 6($t0) i.e. 6th index
+sb $t1, 6($t0)
+
+#00296846@, $t1: 4 , $t2: 9
+#loading the byte in the memory address 5($t0) i.e. 5th index to the register $t1
+lb $t1, 5($t0)
+
+#storing the byte from register $t2 to the memory address 5($t0) i.e. 5th index
+sb $t2, 5($t0)
+
+#loading the byte in the memory address 4($t0) i.e. 4th index to register $t2
+lb $t2, 4($t0)
+
+#storing the byte from register $t1 to the memory address 4($t0) i.e. 4th index
+sb $t1, 4($t0)
+

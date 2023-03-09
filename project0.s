db84b95f444b0ec541c30d6715a71349c3eab7f6
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2023-03-08 20:14:25 -0500

    Update project0.s
    we loaded the elements in the 1 and 2 indices (2 and 9) to the registers $t1 and $t2. we replaced the 2 with 9. And the value 2 is preserved in the register $t1. We loaded the $t2 register then with the last element i.e. 0 and we replace it with the element 2 that was stored in $t1 earlier. Also added \n at the end of the id as it was printing in the same line

--- project0.s
+++ project0.s
@@ -1,11 +1,9 @@
 .data
 
 #storing my student id in the variable id
-id: .asciiz “@02968946”
+id: .asciiz “@02968946\n”
 
 .text
-
-
 #loading the memory address of the id to the register $t0
 la $t0, id
 
@@ -143,3 +141,18 @@ li $v0,4
 la $a0, id
 syscall
 
+#commit
+#2968946@0
+
+#loading the byte in the memory address 0($t0) and 1($t0) -- 1st and 2nd indices-- to register $t1 and $t2
+lb $t1, 0($t0)
+lb $t2, 1($t0)
+
+#storing byte from the register $t2 to the memory address 0($t0) i.e. 0th index
+sb $t2, 0($t0)
+
+#loading the byte in the memory address 8($t0) i.e 8th index to register $t2
+lb $t2, 8($t0)
+
+#storing the byte from register $t1 to the memory address 8($t0) i.e. 8th index
+sb $t1, 8($t0)

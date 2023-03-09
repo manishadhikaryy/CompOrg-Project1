639409536351eb4ad46dd1b377549bce348988a5
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2023-03-08 21:09:26 -0500

    Update project0.s
    I removed all the comments as they were repetitive and making the code unorganized. What we did in the first section to move each element by one digit to the left is what we follow throughout the program.

--- project0.s
+++ project0.s
@@ -75,65 +75,35 @@ syscall
 ##commit
 ##02968946@
 
-#loading the byte in the memory address 0($t0) and 1($t0) -- 1st and 2nd indices-- to register $t1 and $t2
 lb $t1, 0($t0)
 lb $t2, 1($t0)
-
-#storing byte from the register $t2 to the memory address 0($t0) i.e. 0th index
 sb $t2, 0($t0)
-
-#loading the byte in the memory address 8($t0) i.e 8th index to register $t2
 lb $t2, 8($t0)
-
-#storing the byte from register $t1 to the memory address 8($t0) i.e. 8th index
 sb $t1, 8($t0)
 
 #commit
 ##229689460
 
-#loading the byte in the memory address 7($t0) i.e. 7th index to register $t1
 lb $t1, 7($t0)
-
-#storing the byte from register $t2 to the memory address 7($t0) i.e. 7th index
 sb $t2, 7($t0)
-
-#loading the byte in the memory address 6($t0) i.e. 6th index to the register $t2
 lb $t2, 6($t0)
-
-#storing the byte from register $t1 to the memory address 6($t0) i.e. 6th index
 sb $t1, 6($t0)
-
-#loading the byte in the memory address 5($t0) i.e. 5th index to the register $t1
 lb $t1, 5($t0)
 
 #commit
 ##2296896@0
 
-#storing the byte from register $t2 to the memory address 5($t0) i.e. 5th index
 sb $t2, 5($t0)
-
-#loading the byte in the memory address 4($t0) i.e. 4th index to register $t2
 lb $t2, 4($t0)
-
-#storing the byte from register $t1 to the memory address 4($t0) i.e. 4th index
 sb $t1, 4($t0)
-
-#loading the byte in the memory address 3($t0) i.e 3rd index to register $t1
 lb $t1, 3($t0)
-
-#storing the byte from register $t2 to the memory address 3($t0) i.e 3rd index
 sb $t2, 3($t0)
 
 #commit
 #2298946@0
 
-#loading the byte in the memory address 2($t0) i.e. 2nd index to register $t2
 lb $t2, 2($t0)
-
-#storing the byte from register $t1 to the memory address 2($t0) i.e. 2nd index
 sb $t1, 2($t0)
-
-#storing the byte from register $t1 to the memory address 1($t0) i.e. 1st index
 sb $t2, 1($t0)
 
 #print 
@@ -144,15 +114,11 @@ syscall
 #commit
 #2968946@0
 
-#loading the byte in the memory address 0($t0) and 1($t0) -- 1st and 2nd indices-- to register $t1 and $t2
 lb $t1, 0($t0)
 lb $t2, 1($t0)
-
-#storing byte from the register $t2 to the memory address 0($t0) i.e. 0th index
 sb $t2, 0($t0)
-
-#loading the byte in the memory address 8($t0) i.e 8th index to register $t2
 lb $t2, 8($t0)
-
-#storing the byte from register $t1 to the memory address 8($t0) i.e. 8th index
 sb $t1, 8($t0)
+
+#commit
+##9968946@2
